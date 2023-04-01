57f1149fb44997eae942b68408bbf9e94bad560d
theultimatekehop <theultimatekehop@hotmail.com>
2023-03-31 21:43:38 -0400

    dots completed

--- src/app/template/slideshow/slideshow.component.ts
+++ src/app/template/slideshow/slideshow.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.scss']
 })
-
 export class SlideshowComponent implements OnInit {
 
   slides = ['assets/confidentiel.jpg', 'assets/contact-bg.jpg', 'assets/home-bg.jpg'];
@@ -28,10 +27,18 @@ export class SlideshowComponent implements OnInit {
   }
 
   prevSlide(): void {
+    clearInterval(this.slideInterval); // Stop the slideshow
     this.currentSlideIndex = (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
   }
 
   nextSlide(): void {
+    clearInterval(this.slideInterval); // Stop the slideshow
     this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+  }
+
+  goToSlide(index: number): void {
+    clearInterval(this.slideInterval);
+    this.currentSlideIndex = index;
+   // this.stopSlideShow(); // Stop the slideshow when a dot is clicked
   }
 }
